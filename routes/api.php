8bf9d6244b4d88de52a041c28952343a6a4cd671
lembarek <lembarek2011@gmail.com>
2017-02-16 06:26:56 +0100

    add products and show them all

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/product', [
+    'as' => 'product.store',
+    'uses' => 'Api\ProductController@store',
+    ]);
+
+
+Route::get('/products', [
+    'as' => 'product.all',
+    'uses' => 'Api\ProductController@all',
+    ]);
+
+
